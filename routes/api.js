0f18d6425ee0cb4f89a224d2712b25d0a5cc81cb
mazhongyuan <mazhongyuan@byteluck.com>
2025-11-05 11:09:37 +0800

    faet: 前后端接口调通了！！

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/test', (req, res) => {
+  res.send({ message: 'API 测试成功' });
+});
+
+module.exports = router;
